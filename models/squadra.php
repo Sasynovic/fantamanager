fb71fecb5aad78ca1431048b16b2318ab8ff556d
Salvatore Barretta <barrettasalvatore@outlook.it>
2025-04-29 12:32:21 +0200

    feat : visualizzazione squadre in vendita

--- models/squadra.php
+++ models/squadra.php
@@ -17,26 +17,36 @@ class squadra
         $this->conn = $db;
     }
 
-    public function read()
+    public function read($vendita_filter = null)
     {
         $query =  "
-                        SELECT 
-                            s.id,
-                            s.nome_squadra,
-                            pres.nome AS nome_pres,
-                            pres.cognome AS cognome_pres,
-                            vice.nome AS nome_vice,
-                            vice.cognome AS cognome_vice,
-                            st.nome_stadio
-                        FROM " . $this->table_name . " s
-                        LEFT JOIN presidenti pres ON s.id_pres = pres.id
-                        LEFT JOIN presidenti vice ON s.id_vice = vice.id
-                        LEFT JOIN stadio st ON s.id_stadio = st.id
-                        ORDER BY s.id ASC";
+        SELECT 
+            s.id,
+            s.nome_squadra,
+            s.vendita,
+            pres.nome AS nome_pres,
+            pres.cognome AS cognome_pres,
+            vice.nome AS nome_vice,
+            vice.cognome AS cognome_vice,
+            st.nome_stadio
+        FROM " . $this->table_name . " s
+        LEFT JOIN presidenti pres ON s.id_pres = pres.id
+        LEFT JOIN presidenti vice ON s.id_vice = vice.id
+        LEFT JOIN stadio st ON s.id_stadio = st.id";
+
+        if ($vendita_filter !== null) {
+            $query .= " WHERE s.vendita = :vendita";
+        }
+
+        $query .= " ORDER BY s.id ASC";
 
         $stmt = $this->conn->prepare($query);
-        $stmt->execute();
 
+        if ($vendita_filter !== null) {
+            $stmt->bindParam(":vendita", $vendita_filter, PDO::PARAM_INT);
+        }
+
+        $stmt->execute();
         return $stmt;
     }
 }
