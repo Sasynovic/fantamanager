f6e805431d738fd315ea4abe57e237080a6c7b6e
Salvatore Barretta <barrettasalvatore@outlook.it>
2025-05-05 16:48:59 +0200

    feat: aggiunta annualita' alle competizioni

--- models/competizione.php
+++ models/competizione.php
@@ -21,12 +21,16 @@ class competizione
                             c.id, 
                             c.id_divisione AS id_divisione, 
                             c.nome_competizione, 
-                            d.nome_divisione
+                            
+                            d.nome_divisione,
+                            ss.stagione AS anno
                         FROM " . $this->table_name . " c
                         LEFT JOIN divisione d ON c.id_divisione = d.id
+                        LEFT JOIN stagioni_sportive ss ON c.anno = ss.id
+                        WHERE ss.attiva = 1
                         ";
         if ($id_divisione) {
-            $query .= " WHERE c.id_divisione = :id_divisione";
+            $query .= " AND c.id_divisione = :id_divisione";
         }
         $query .= " ORDER BY c.id ASC";
 
