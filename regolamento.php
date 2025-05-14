fe2880c69e8fdd45e31e639fc91389c91990bb1c
Salvatore Barretta <barrettasalvatore@outlook.it>
2025-05-15 01:38:09 +0200

    feat : inserita pagina regolamento

---
+++ regolamento.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fantacalcio Manageriale</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />
+    <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+
+    <style>
+        .main-body{height: 93%;}
+    </style>
+</head>
+<div class="main-container">
+    <aside class="main-menu">
+        <div class="menu-header">
+            <h1>Fantacalcio</h1>
+            <h3>Manageriale</h3>
+        </div>
+
+        <ul class="menu-list">
+            <li class="menu-item">
+                <a href="index.php">Dashboard</a>
+            </li>
+            <li class="menu-item">
+                <a href="albo.php">Albo d'oro</a>
+            </li>
+            <li class="menu-item">
+                <a href="index.php">Squadre in vendita</a>
+            </li>
+            <li class="menu-item">
+                <a href="tool.php">Tool scambi</a>
+            </li>
+            <li class="menu-item">
+                <a href="regolamento.php">Regolamento</a>
+            </li>
+            <li class="menu-item">
+                <a href="index.php">Ricerca</a>
+            </li>
+            <li class="menu-item">
+                <a href="index.php">News</a>
+            </li>
+            <li class="menu-item">
+                <a href="index.php">Contatti</a>
+            </li>
+        </ul>
+    </aside>
+
+    <div class="main-content">
+        <header class="main-header">
+            <div class="main-text-header">
+                <button class="back-button" onclick="window.history.back();">
+                    <img src="chevronL.svg" alt="Indietro" height="40px" width="40px">
+                </button>
+                <h1>Tool Scambi</h1>
+                <a href="admin/login.php">Admin</a>
+            </div>
+        </header>
+
+
+        <div class="main-body">
+            <div class="main-body-content" id="main-body-content">
+            <div style="position:relative;padding-top:0;width:100%;height:100%;">
+                <iframe style="position:absolute;border:none;width:100%;height:100%;left:0;top:0;" src="https://online.fliphtml5.com/ymmet/rgxe/"  seamless="seamless" scrolling="no" frameborder="0" allowtransparency="true" allowfullscreen="true" ></iframe>
+            </div>
+            </div>
+        </div>
