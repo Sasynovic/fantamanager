9bab453e0999539f7f7ade2645363f02653d64a9
Salvatore Barretta <barrettasalvatore@outlook.it>
2025-05-16 21:21:49 +0200

    fear: prima implementazione ricerca

---
+++ ricerca.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fantacalcio Manageriale</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />
+    <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+    <script src="renderFooter.js" defer></script>
+    <script src="showmenu.js" defer></script>
+
+</head>
+<div class="main-container">
+    <aside class="main-menu">
+        <div class="menu-header">
+            <h1>Fantacalcio</h1>
+            <h3>Manageriale</h3>
+        </div>
+
+        <ul class="menu-list">
+            <li class="menu-item">
+                <a href="index.php">Dashboard</a>
+            </li>
+            <li class="menu-item">
+                <a href="albo.php">Albo d'oro</a>
+            </li>
+            <li class="menu-item">
+                <a href="vendita.php">Squadre in vendita</a>
+            </li>
+            <li class="menu-item">
+                <a href="tool.php">Tool scambi</a>
+            </li>
+            <li class="menu-item">
+                <a href="regolamento.php">Regolamento</a>
+            </li>
+            <li class="menu-item">
+                <a href="ricerca.php">Ricerca</a>
+            </li>
+            <li class="menu-item">
+                <a href="contatti.php">Contatti</a>
+            </li>
+        </ul>
+    </aside>
+
+    <div class="main-content">
+        <header class="main-header">
+            <div class="main-text-header">
+                <button class="back-button" onclick="window.history.back();">
+                    <img src="chevronL.svg" alt="Indietro" height="40px" width="40px">
+                </button>
+                <h1>Ricerca</h1>
+                <h1 id="hamburger-menu">≡</h1>
+            </div>
+        </header>
+
+
+        <div class="main-body">
+            <div class="main-body-content" id="main-body-content">
+                <div class="filter">
+                    <h2>Filtri di ricerca</h2>
+                    <div class="filter-item">
+                        <label for="team">Squadra:</label>
+                        <input type="text" id="team" placeholder="Inserisci il nome della squadra">
+                    </div>
+                    <div class="filter-item">
+                        <label for="player">Giocatore:</label>
+                        <input type="text" id="player" placeholder="Inserisci il nome del presidente">
+                    </div>
+                    <button id="search-button">Cerca</button>
+            </div>
+        </div>
+
+        <footer class="main-footer">
+            <div class="swiper-container footer-swiper">
+                <div class="swiper-wrapper" id="footerList">
+                    <!-- Gli elementi division-ball verranno inseriti qui tramite JavaScript -->
+                </div>
+                <!-- Aggiunti i pulsanti di navigazione -->
+                <div class="swiper-button-prev footer-nav-prev">
+                    <img src="chevronL.svg" alt="Indietro">
+                </div>
+                <div class="swiper-button-next footer-nav-next">
+                    <img src="chevronR.svg" alt="Avanti">
+                </div>
+            </div>
+        </footer>
+    </div>
+</div>
+
+<script>
+
+
+
+</script>
+
+
+
